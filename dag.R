dc715c48911bede7d280f64ace53baec7db536e5
Pedro Cardoso <pedrocarsamar@gmail.com>
2024-03-22 11:15:12 -0300

    Updates.

--- dag.R
+++ dag.R
@@ -1,5 +1,3 @@
-install.packages('dagitty')
-
 library(dplyr)
 library(dagitty)
 library(ggdag)
@@ -9,14 +7,12 @@ theme_set(theme_dag())
 
 #controles demograficos: urbanizacao, gastos militares
 
-tot_welfare <- dagify(
-  welfare ~ tax + open + left + unitconfounder + dependent + unemp + inflation +
-    debt + mob,
+dag1 <- dagify(
+  welfare ~ tax + open + left + unitconfounder + dependent + unemp + inflation + debt,
   inflation ~ debt,
   tax ~ tot + capacity + unemp,
   tot ~ trade + commodity + unitconfounder,
   trade ~ open,
-  left ~ unemp,
   exposure = "tot",
   outcome = "welfare",
   latent = "capacity",
@@ -40,11 +36,12 @@ tot_welfare <- dagify(
                 y = c(tot = 0, 
                       open = 0.2, capacity = 0.1, left = 0.1, trade = 0.1, 
                       tax = 0, welfare = 0, commodity = -0.1,
-                      unitconfounder = -0.1, dependent = -0.1,
+                      unitconfounder = -0.3, dependent = -0.1,
                       unemp = -0.1, inflation = 0.1, debt = 0.2,
                       mob = -0.1)))
 
-tot_welfare %>%
+
+dag1 %>%
   tidy_dagitty() %>% 
   mutate(linetype = ifelse(name %in% c("unitconfounder", "mob"), "dashed", "solid")) %>%
   ggplot(aes(x = x, y = y, xend = xend, yend = yend)) + 
@@ -53,14 +50,40 @@ tot_welfare %>%
   geom_dag_label_repel(aes(label = label), show.legend = F)
 
 
-dagitty::paths(tot_welfare)
+dag2 <- dagify(
+  welfare ~ tax + open + unitconfounder + war1 + neutral,
+  tax ~ tot + capacity + unitconfounder,
+  tot ~ unitconfounder + open + war1,
+  war2 ~ welfare,
+  exposure = "tot",
+  outcome = "welfare",
+  latent = "capacity",
+  labels = c(welfare = "Social Spending", tot = "Terms of Trade", 
+             tax = "Tax Revenue",
+             open = "Trade Openness",
+             capacity = "State Capacity",
+             commodity = "Commodity Prices",
+             unitconfounder = "Unit Unobserved Confounder",
+             war1 = "Warfare T1",
+             war2 = "Warfare T2",
+             neutral = "Neutral Controls"),
+  coords = list(x = c(tot = 0, open = 0.7, unitconfounder = 0.7,
+                      capacity = 0.7, tax = 0.7, 
+                      welfare = 1.4, commodity = 0, war1 = 0.7, 
+                      war2 = 1.6, neutral = 1.11),
+                y = c(tot = 0, neutral = -0.1,
+                      open = 0.2, capacity = 0.1, 
+                      tax = 0, welfare = 0, commodity = -0.1,
+                      unitconfounder = -0.1, war1 = 0.3, war2 = 0.2)
+                )
+  )
 
-gg1 <- tot_welfare %>%
+gg2 <- dag2 %>%
   tidy_dagitty() %>% 
   mutate(linetype = ifelse(name %in% c("unitconfounder", "mob"), "dashed", "solid")) %>%
   ggplot(aes(x = x, y = y, xend = xend, yend = yend)) + 
   geom_dag_point() + 
   geom_dag_edges(aes(edge_linetype = linetype), show.legend = FALSE) +
-  geom_dag_label_repel(aes(label = label), show.legend = F)
+  geom_dag_label(aes(label = label), show.legend = F)
 
-ggsave('product/dag.jpeg', dpi = 300, height = 5, width = 10, unit = 'in', gg1)
+ggsave('product/dag.jpeg', dpi = 300, height = 5, width = 10, unit = 'in', gg2)
